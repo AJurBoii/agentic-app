3c2ea79569f4c537701b8e8af891ced2685e5346
AJ <amariusj.lesure@gmail.com>
2026-03-05 10:49:27 -0600

    Add test for linked list append method

--- test_ds.py
+++ test_ds.py
@@ -1,10 +1,18 @@
 import unittest
 
-from main import bubble_sort, calculate_average, greet
+from data_structures_practice import hash_table, linked_list
 
 
 class TestMain(unittest.TestCase):
-    pass
+    def test_linked_list(self):
+        ll = linked_list.LinkedList()
+        ll.append(1)
+        ll.append(2)
+        ll.append(3)
+
+        assert ll.head.value == 1
+        assert ll.head.next.value == 2
+        assert ll.head.next.next.value == 3
 
 
 if __name__ == "__main__":
